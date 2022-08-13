8280b09c5d887b6e36215f7a13c5d42c5ece98b0
johnyguido <johnyguido@hotmail.com.br>
2022-08-13 01:27:23 -0300

    Innitial Commit

---
+++ src/main/java/com/cs/acompanhamentotwo/AcompanhamentoTwoApplication.java
@@ -0,0 +1,13 @@
+package com.cs.acompanhamentotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhamentoTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcompanhamentoTwoApplication.class, args);
+    }
+
+}
