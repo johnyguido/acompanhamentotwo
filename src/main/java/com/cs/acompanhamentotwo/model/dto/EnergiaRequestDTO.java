52ea75a7cb77e14122decf00f75a2ad73bd9f48b
johnyguido <johnyguido@hotmail.com.br>
2022-08-23 23:28:20 -0300

    Ajuste POM Model Mapper e Lombok para versao que compila no IntelliJ

--- src/main/java/com/cs/acompanhamentotwo/model/dto/EnergiaRequestDTO.java
+++ src/main/java/com/cs/acompanhamentotwo/model/dto/EnergiaRequestDTO.java
@@ -1,8 +1,14 @@
 package com.cs.acompanhamentotwo.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnergiaRequestDTO {
 
     private Long leituraFinal;
